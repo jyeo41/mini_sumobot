aca8261963afbc3e721570104a967e51ed43dec5
jyeo41 <jyeo41@gmail.com>
2024-10-25 14:45:42 -0400

    fix(gpio hal): fix logic in gpio_mode_set function to update struct
    Originally the gpio_mode_set() function was only setting the bits in the
    appropriate registers to set the mode of the pin. The function was not
    updating the value of the attribute inside of the struct itself, so this
    means later when gpio_config_compare() function is used, the config
    would return the wrong value since it has not been updated.

--- Src/gpio.c
+++ Src/gpio.c
@@ -7,8 +7,13 @@
 /* Array to map high level pin names (an enum) such as LED_GREEN to the pin's attributes (a struct).
  * This centralizes all the info one would need about a pin, such as the port, pin number, etc.
  * This array is the core implementation of abstracting away the low level register interactions from the user.
+ *
+ * These are the default pin configurations the board starts with, and using gpio functions such as
+ *  - gpio_mode_set()
+ *
+ * The application can change some of the attributes at runtime. The pin name, port, and pin number should be immutable.
  */
-static const gpio_pin_t gpio_board_pins[] = {
+static gpio_pin_t gpio_board_pins[] = {
     [LED_GREEN] =   {LED_GREEN, GPIOD, 12, GPIO_MODE_OUTPUT},
     //[LED_ORANGE] =  {LED_ORANGE, GPIOD, 13, GPIO_MODE_OUTPUT},
     [LED_RED] =     {LED_RED, GPIOD, 14, GPIO_MODE_OUTPUT},
@@ -60,11 +65,13 @@ void gpio_default_initialize(void)
     }
 }
 
-bool gpio_config_compare(gpio_pin_names_e pin_to_check,
-			 gpio_pin_names_e expected_pin_name,
-			 const GPIO_TypeDef* expected_port,
-			 uint8_t expected_pin_number,
-			 uint8_t expected_mode)
+/* Make all the incoming parameters constants because they should be read only.
+ * This is to explicitly state these values are to be only used for comparison purposes. */
+bool gpio_config_compare(const gpio_pin_names_e pin_to_check,
+			 const gpio_pin_names_e expected_pin_name,
+			 const GPIO_TypeDef* const expected_port,
+			 const uint8_t expected_pin_number,
+			 const uint8_t expected_mode)
 {
     const gpio_pin_t test_pin = gpio_board_pins[pin_to_check];
 
@@ -74,25 +81,33 @@ bool gpio_config_compare(gpio_pin_names_e pin_to_check,
             (test_pin.mode == expected_mode);
 }
 
-void gpio_mode_set(gpio_pin_names_e pin_name, gpio_mode_e mode)
+void gpio_mode_set(gpio_pin_names_e pin_name, gpio_mode_e desired_mode)
 {
-    const gpio_pin_t pin = gpio_board_pins[pin_name];
+    gpio_pin_t pin = gpio_board_pins[pin_name];
     
-    switch(mode) {
+    switch(desired_mode) {
+        /* Input mode is 0b00 */
         case GPIO_MODE_INPUT:
-            //pin.port->MODER &= ~(pin.pin_number) & ~(pin.pin_number << 1);
+            pin.port->MODER &= ~(gpio_mode_register_bits[pin.pin_number][0]);
+            pin.port->MODER &= ~(gpio_mode_register_bits[pin.pin_number][1]);
             break;
+        /* GPIO Output mode is 0b01 */
         case GPIO_MODE_OUTPUT:
             pin.port->MODER |= gpio_mode_register_bits[pin.pin_number][0];
             pin.port->MODER &= ~(gpio_mode_register_bits[pin.pin_number][1]);
             break;
+        /* Alternate function mode is 0b10 */
         case GPIO_MODE_ALTERNATE:
-            //pin.port->MODER |= (pin.pin_number << 1);
+            pin.port->MODER &= ~(gpio_mode_register_bits[pin.pin_number][0]);
+            pin.port->MODER |= gpio_mode_register_bits[pin.pin_number][1];
             break;
+        /* Analog mode is 0b11 */
         case GPIO_MODE_ANALOG:
-            //pin.port->MODER |= pin.pin_number | (pin.pin_number << 1);
+            pin.port->MODER |= gpio_mode_register_bits[pin.pin_number][0];
+            pin.port->MODER |= gpio_mode_register_bits[pin.pin_number][1];
             break;
     }
+    gpio_board_pins[pin_name].mode = desired_mode;
 }
 
 /* Atomic write to the data register for GPIO Output pins.
